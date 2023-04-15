e098bd0eb2846daa6d50ec238c73365dd9ccedbd
Steven Zhou <zz475@cornell.edu>
2023-04-15 19:22:21 -0400

    Fin-C: coded image modal js

---
+++ scripts/projects_img_modal.js
@@ -0,0 +1,73 @@
+$(".modal_close").click(function() {
+    $(".modal").addClass("hidden");
+  });
+
+$("#p1_fig2-1_button").click(function() {
+    $(".modal_image").addClass("hidden");
+    $(".modal, #p1_fig2-1_large").removeClass("hidden");
+  });
+
+$("#p1_fig2-4_button").click(function() {
+    $(".modal_image").addClass("hidden");
+    $(".modal, #p1_fig2-4_large").removeClass("hidden");
+  });
+
+$("#p1_fig2-6_button").click(function() {
+    $(".modal_image").addClass("hidden");
+    $(".modal, #p1_fig2-6_large").removeClass("hidden");
+  });
+
+$("#p1_fig2-7_button").click(function() {
+    $(".modal_image").addClass("hidden");
+    $(".modal, #p1_fig2-7_large").removeClass("hidden");
+  });
+
+$("#p1_fig3-1_button").click(function() {
+    $(".modal_image").addClass("hidden");
+    $(".modal, #p1_fig3-1_large").removeClass("hidden");
+  });
+
+$("#p1_fig3-2_button").click(function() {
+    $(".modal_image").addClass("hidden");
+    $(".modal, #p1_fig3-2_large").removeClass("hidden");
+  });
+
+$("#p1_fig3-3_button").click(function() {
+    $(".modal_image").addClass("hidden");
+    $(".modal, #p1_fig3-3_large").removeClass("hidden");
+  });
+
+$("#p1_fig3-4_button").click(function() {
+    $(".modal_image").addClass("hidden");
+    $(".modal, #p1_fig3-4_large").removeClass("hidden");
+  });
+
+$("#p1_fig3-5_button").click(function() {
+    $(".modal_image").addClass("hidden");
+    $(".modal, #p1_fig3-5_large").removeClass("hidden");
+  });
+
+$("#p1_fig3-6_button").click(function() {
+    $(".modal_image").addClass("hidden");
+    $(".modal, #p1_fig3-6_large").removeClass("hidden");
+  });
+
+$("#p1_fig3-7_button").click(function() {
+    $(".modal_image").addClass("hidden");
+    $(".modal, #p1_fig3-7_large").removeClass("hidden");
+  });
+
+$("#p1_fig3-10_button").click(function() {
+    $(".modal_image").addClass("hidden");
+    $(".modal, #p1_fig3-10_large").removeClass("hidden");
+  });
+
+$("#p1_fig3-11_button").click(function() {
+    $(".modal_image").addClass("hidden");
+    $(".modal, #p1_fig3-11_large").removeClass("hidden");
+  });
+
+$("#p1_fig3-12_button").click(function() {
+    $(".modal_image").addClass("hidden");
+    $(".modal, #p1_fig3-12_large").removeClass("hidden");
+  });
