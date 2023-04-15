ffe46add65d7cea6251bbdb998ad6e8e20efb3d7
Steven Zhou <zz475@cornell.edu>
2023-04-15 16:23:39 -0400

    Fin-C: coded accordian

---
+++ scripts/experience_accordian.js
@@ -0,0 +1,77 @@
+$("#work_rna_b").click(function() {
+    if ($("#work_rna_p").hasClass("hidden")) {
+        $(".accordian_body, .dropdown_arrow_expanded").addClass("hidden");
+        $(".dropdown_arrow").removeClass("hidden");
+
+        $("#work_rna_b > .dropdown_arrow").addClass("hidden");
+        $("#work_rna_b > .dropdown_arrow_expanded, #work_rna_p").removeClass("hidden");
+      } else {
+        $(".accordian_body, .dropdown_arrow_expanded").addClass("hidden");
+        $(".dropdown_arrow").removeClass("hidden");
+      }
+  });
+
+$("#work_u_b").click(function() {
+    if ($("#work_u_p").hasClass("hidden")) {
+        $(".accordian_body, .dropdown_arrow_expanded").addClass("hidden");
+        $(".dropdown_arrow").removeClass("hidden");
+
+        $("#work_u_b > .dropdown_arrow").addClass("hidden");
+        $("#work_u_b > .dropdown_arrow_expanded, #work_u_p").removeClass("hidden");
+      } else {
+        $(".accordian_body, .dropdown_arrow_expanded").addClass("hidden");
+        $(".dropdown_arrow").removeClass("hidden");
+      }
+  });
+
+$("#work_e_b").click(function() {
+    if ($("#work_e_p").hasClass("hidden")) {
+        $(".accordian_body, .dropdown_arrow_expanded").addClass("hidden");
+        $(".dropdown_arrow").removeClass("hidden");
+
+        $("#work_e_b > .dropdown_arrow").addClass("hidden");
+        $("#work_e_b > .dropdown_arrow_expanded, #work_e_p").removeClass("hidden");
+      } else {
+        $(".accordian_body, .dropdown_arrow_expanded").addClass("hidden");
+        $(".dropdown_arrow").removeClass("hidden");
+      }
+  });
+
+$("#work_a_b").click(function() {
+    if ($("#work_a_p").hasClass("hidden")) {
+        $(".accordian_body, .dropdown_arrow_expanded").addClass("hidden");
+        $(".dropdown_arrow").removeClass("hidden");
+
+        $("#work_a_b > .dropdown_arrow").addClass("hidden");
+        $("#work_a_b > .dropdown_arrow_expanded, #work_a_p").removeClass("hidden");
+      } else {
+        $(".accordian_body, .dropdown_arrow_expanded").addClass("hidden");
+        $(".dropdown_arrow").removeClass("hidden");
+      }
+  });
+
+$("#work_c_b").click(function() {
+    if ($("#work_c_p").hasClass("hidden")) {
+        $(".accordian_body, .dropdown_arrow_expanded").addClass("hidden");
+        $(".dropdown_arrow").removeClass("hidden");
+
+        $("#work_c_b > .dropdown_arrow").addClass("hidden");
+        $("#work_c_b > .dropdown_arrow_expanded, #work_c_p").removeClass("hidden");
+      } else {
+        $(".accordian_body, .dropdown_arrow_expanded").addClass("hidden");
+        $(".dropdown_arrow").removeClass("hidden");
+      }
+  });
+
+$("#work_p_b").click(function() {
+    if ($("#work_p_p").hasClass("hidden")) {
+        $(".accordian_body, .dropdown_arrow_expanded").addClass("hidden");
+        $(".dropdown_arrow").removeClass("hidden");
+
+        $("#work_p_b > .dropdown_arrow").addClass("hidden");
+        $("#work_p_b > .dropdown_arrow_expanded, #work_p_p").removeClass("hidden");
+      } else {
+        $(".accordian_body, .dropdown_arrow_expanded").addClass("hidden");
+        $(".dropdown_arrow").removeClass("hidden");
+      }
+  });
